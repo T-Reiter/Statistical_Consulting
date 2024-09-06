c50e099915579018f65811777db82abe508a1986
thomasreiter <T.Reiter@psy.lmu.de>
2024-09-06 09:30:41 +0200

    updates with additional covariates

--- Analysis_cjbart.R
+++ Analysis_cjbart.R
@@ -11,6 +11,34 @@ library(cjbart)
 ### Load Data 
 cj_tidy <- readRDS("Ukraine Conjoint Data/cj_tidy.rds")
 
+# get additional covariate data 
+add_covars <- 
+  readRDS("Ukraine Conjoint Data/CJ_Ukraine_NewCovariates.Rds")
+# get additional covariate data --> same data different files types?
+library(haven)
+add_covars2 <- 
+  haven::read_dta("Ukraine Conjoint Data/CJ_Ukraine_NewCovariates.dta")
+rm(add_covars2)
+
+
+### Join the add_covars with the cj_tidy data 
+# for this, create c_id (country_id) variable in add_covars
+add_covars$country_id <- paste0(add_covars$Country, 
+                                "_",
+                                add_covars$ID)
+# Remove the covars in add_covars that are already present in cj_tidy
+which(!colnames(add_covars) %in% colnames(cj_tidy))
+add_covars_new <- add_covars[,which(!colnames(add_covars) %in% 
+                                      colnames(cj_tidy))]
+
+cj_tidy <- merge(cj_tidy,
+                 add_covars_new,
+                 by.x = "c_id",
+                 by.y = "country_id", 
+                 all.x = T) # left join
+
+
+
 # Rename Attributes
 colnames(cj_tidy)[which(colnames(cj_tidy) %in% c(paste0("attr",1:9)))] <-
   c("Sold_killed_UKR",
@@ -22,6 +50,17 @@ colnames(cj_tidy)[which(colnames(cj_tidy) %in% c(paste0("attr",1:9)))] <-
     "Risk_Nuke",
     "Territ_Cession",
     "Polit_Self_Det_UKR")
+# get cols as characters(needed for imces)
+# cj_tidy$Sold_killed_UKR <- as.character(cj_tidy$Sold_killed_UKR)
+# cj_tidy$Sold_killed_RUS <- as.character(cj_tidy$Sold_killed_RUS)
+# cj_tidy$Civ_killed_UKR <- as.character(cj_tidy$Civ_killed_UKR)
+# cj_tidy$Infra_Destr_UKR <- as.character(cj_tidy$Infra_Destr_UKR)
+# cj_tidy$Perc_GDP_milit <- as.character(cj_tidy$Perc_GDP_milit)
+# cj_tidy$Perc_GDP_econ <- as.character(cj_tidy$Perc_GDP_econ)
+# cj_tidy$Risk_Nuke <- as.character(cj_tidy$Risk_Nuke)
+# cj_tidy$Territ_Cession <- as.character(cj_tidy$Territ_Cession)
+# cj_tidy$Polit_Self_Det_UKR <- as.character(cj_tidy$Polit_Self_Det_UKR)
+
 
 
 ### Fit
@@ -31,11 +70,11 @@ fit_cjbart <- cjbart(data = cj_tidy,
                      id = "c_id", 
                      round = "task", 
                      use_round = TRUE, 
-                     cores = 3)
+                     cores = 4)
 
 
-saveRDS(fit_cjbart, "fit_cjbart.rds")
-fit_cjbart <- readRDS("fit_cjbart.rds")
+saveRDS(fit_cjbart, "fit_cjbart_add_covars.rds")
+fit_cjbart <- readRDS("fit_cjbart_add_covars.rds")
 
 
 # Get IMCEs
@@ -43,8 +82,7 @@ t1 <- Sys.time()
 imces <- IMCE(data = cj_tidy,
               keep_omce = TRUE,
               model = fit_cjbart, 
-              attribs = c(
-                            "Sold_killed_UKR"
+              attribs = c("Sold_killed_UKR"
                           , "Sold_killed_RUS"
                           , "Civ_killed_UKR"
                           , "Infra_Destr_UKR"
@@ -52,10 +90,9 @@ imces <- IMCE(data = cj_tidy,
                           , "Perc_GDP_econ"
                           , "Risk_Nuke"
                           , "Territ_Cession"
-                          , "Polit_Self_Det_UKsR"
+                          , "Polit_Self_Det_UKR"
                           ), 
-              ref_levels = c(
-                               '25,000'
+              ref_levels = c('25,000'
                              , '25,000.'
                              , '4,000'
                              , '50B'
@@ -70,37 +107,33 @@ imces <- IMCE(data = cj_tidy,
 )
 t2 <- Sys.time()
 
-saveRDS(imces, "imces_all_new.rds")
-imces <- readRDS("imces_all_new.rds")
+saveRDS(imces, "imces_all_new_add_covars.rds")
+# imces <- readRDS("imces_all_new_add_covars.rds")
 
 
 # Get VarImps
+covars_of_interest <- c("Country", "age", "gender", "leftright3",
+                        # additional covariates
+                        "QV_Ukraine", "q3_3", "Q5", "q1617_USA",
+                        "q1617_RUS", "Q11", "Q15_1", "Q15_7",
+                        "q10_2", "q10_6", "q10_8", "q10_10"
+                        )
+
 var_imps <- het_vimp(imces = imces, 
-                     covars = c("Country", 
-                                "age", 
-                                "gender", 
-                                "leftright3"), 
+                     covars = covars_of_interest, 
                      cores = 4)
-  
+
          
-saveRDS(var_imps, "var_imps_all_attributes_new.rds")
-var_imps <- readRDS("var_imps_all_attributes_new.rds")
+saveRDS(var_imps, "var_imps_all_attributes_new_add_covars.rds")
+# var_imps <- readRDS("var_imps_all_attributes_new_add_covars.rds")
 
 # Get Plot of VarImps (as in Paper)
 test <- plot(var_imps)
 
 # Change order of the factors --> ToDo
 
-# 
-library(ggplot2)
-test +
-  ggtitle("TEst")
   
-
-
-
-
-# Fit single 
+# Fit single trees 
 library(rpart)
 library(rpart.plot) # standard built-in plot
 
@@ -144,7 +177,7 @@ rpart.plot(fit)
 
 
 #### Custom plot
-x <- var_imps
+plot_data <- x$results
 
 
 # Generate Custom Plot Attribute Labels with Line Breaks
@@ -190,7 +223,7 @@ ggplot2::ggplot(plot_data,
                                            ),
                  panel.background = ggplot2::element_rect(fill = "white")) 
 
-
+ggsave("var_imps_add_covars.png")
 
 
 
